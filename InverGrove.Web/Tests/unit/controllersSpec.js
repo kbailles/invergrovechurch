0f08c109350b2260574d7b8993238e1442dec690
kbailles <kbailles@outlook.com>
2014-09-18 21:24:42 -0500

    First passing unit test

--- InverGrove.Web/Tests/unit/controllersSpec.js
+++ InverGrove.Web/Tests/unit/controllersSpec.js
@@ -1,14 +1,32 @@
+
+/// <reference path="../dependencies/jasmine.js" />
+
+/// <reference path="../../Components/angular/angular.js" />
+/// <reference path="../../Components/angular-mocks/angular-mocks.js" />
+/// <reference path="../../Components/angular-route/angular-route.js" />
+
+/// <reference path="../../Components/angular-bootstrap/ui-bootstrap.js" />
+
+/// <reference path="../../Scripts/app.js" />
+/// <reference path="../../Scripts/common/services.js" />
+/// <reference path="../../Scripts/common/factories.js" />
+/// <reference path="../../Scripts/common/directives.js" />
+/// <reference path="../../Scripts/common/filters.js" />
+
+/// <reference path="../../Scripts/controllers/AppController.js" />
+/// <reference path="../../Scripts/controllers/HomeController.js" />
+
 'use strict';
 
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+    beforeEach(module('app.controllers'));
+    beforeEach(module('app.services'));
 
-
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('HomeCtrl', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
-  }));
+    it('should ....', inject(function ($controller) {
+        //spec body
+        var myCtrl1 = $controller('HomeCtrl', { $scope: {} });
+        expect(myCtrl1).toBeDefined();
+    }));
 });
