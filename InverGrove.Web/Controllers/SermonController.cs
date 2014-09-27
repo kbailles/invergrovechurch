93632cec6a80fd2ca7786230d35ab68a366cdbac
kbailles <kbailles@outlook.com>
2014-09-27 17:32:21 -0500

    Added sermons page. Still under construction

---
+++ InverGrove.Web/Controllers/SermonController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace InverGrove.Web.Controllers
+{
+    public class SermonController : Controller
+    {
+        [HttpGet]
+        public ActionResult ViewAll()
+        {
+            return View("_Sermons");
+        }
+    }
+}
