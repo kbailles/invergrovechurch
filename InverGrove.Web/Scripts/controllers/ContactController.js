eec7daa05c4abea268e388b0913d9d2a00e5b9ca
kbailles <kbailles@outlook.com>
2014-10-01 22:05:31 -0500

    Removed uneeded white space

--- InverGrove.Web/Scripts/controllers/ContactController.js
+++ InverGrove.Web/Scripts/controllers/ContactController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app.controllers')
-    
+
     .controller('ContactCtrl', ['$scope', '$http', 'LocationFactory', function ($scope, $http, locationFactory) {
         $scope.churchLocation = locationFactory;
         $scope.working = false;
