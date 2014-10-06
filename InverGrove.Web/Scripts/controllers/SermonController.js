52ff22c40b22d646d6ebc4904305d6520c9f08a7
kbailles <kbailles@outlook.com>
2014-10-05 22:29:46 -0500

    Checking in work I have for sermon page.
    Filtering via the search input now works

--- InverGrove.Web/Scripts/controllers/SermonController.js
+++ InverGrove.Web/Scripts/controllers/SermonController.js
@@ -3,14 +3,58 @@
 angular.module('app.controllers')
 
     .controller('SermonCtrl', ['$scope', '$http', function ($scope, $http) {
-        var fakeData = [
+
+        var fakeSermons = [
             {
-                title: 'test1'
+                date: new Date(2014, 9, 26),
+                sermonId: 5,
+                speaker: 'Antoine Halloway',
+                tags: [
+                    'Be Bold'
+                ],
+                title: 'Boldly Respond to the Invitation'
             },
             {
-                title: 'test2'
+                date: new Date(2014, 9, 25),
+                sermonId: 4,
+                speaker: 'Antoine Halloway',
+                tags: [
+                    'Be Bold',
+                    'Test2'
+                ],
+                title: 'Boldly Trust Jesus'
+            },
+            {
+                date: new Date(2014, 9, 24),
+                sermonId: 3,
+                speaker: 'Antoine Halloway',
+                tags: [
+                    'Be Bold',
+                    'Test1'
+                ],
+                title: 'Speak Boldly'
+            },
+            {
+                date: new Date(2014, 9, 23),
+                sermonId: 2,
+                speaker: 'Antoine Halloway',
+                tags: [
+                    'Be Bold',
+                    'Test1'
+                ],
+                title: 'Boldly Believe in the Son of God'
+            },
+            {
+                date: new Date(2014, 9, 22),
+                sermonId: 1,
+                speaker: 'Antoine Halloway',
+                tags: [
+                    'Be Bold',
+                    'Test2'
+                ],
+                title: 'Boldly Seek After God'
             }
         ];
 
-        $scope.sermons = fakeData;
+        $scope.sermons = fakeSermons;
     }]);
