193765bb679b97e48829560b56ca52e1e4f54e7a
kbailles <kbailles@outlook.com>
2014-10-05 20:59:21 -0500

    Updated church logo and sermon controller

--- InverGrove.Web/Scripts/controllers/SermonController.js
+++ InverGrove.Web/Scripts/controllers/SermonController.js
@@ -1,1 +1,16 @@
-﻿
+﻿'use strict';
+
+angular.module('app.controllers')
+
+    .controller('SermonCtrl', ['$scope', '$http', function ($scope, $http) {
+        var fakeData = [
+            {
+                title: 'test1'
+            },
+            {
+                title: 'test2'
+            }
+        ];
+
+        $scope.sermons = fakeData;
+    }]);
