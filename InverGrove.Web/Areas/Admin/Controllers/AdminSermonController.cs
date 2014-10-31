6aa15b9e6f31f0a9f4817e7e0c90325081bd523f
kbailles <kbailles@outlook.com>
2014-10-30 21:59:19 -0500

    Keenan - Missed some files

---
+++ InverGrove.Web/Areas/Admin/Controllers/AdminSermonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using InverGrove.Domain.Exceptions;
+using InverGrove.Domain.Interfaces;
+
+namespace InverGrove.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "MemberAdmin, SiteAdmin")]
+    public class AdminSermonController : Controller
+    {
+        private readonly ISermonService sermonService;
+
+        public AdminSermonController(ISermonService sermonService)
+        {
+            this.sermonService = sermonService;
+        }
+
+        // GET: Admin/Sermon
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Domain.Models.Sermon sermon)
+        {
+            if (sermon == null)
+            {
+                throw new ParameterNullException("sermon");
+            }
+
+            var sermonId = this.sermonService.AddSermon(sermon);
+
+            return this.View();
+        }
+    }
+}
