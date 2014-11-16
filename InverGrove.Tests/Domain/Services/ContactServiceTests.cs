8d61353e0d8f65b84a416151c3fde37b0888386a
lance22me <lancebailles@hotmail.com>
2014-11-15 21:13:35 -0600

    lance - unit test for contact service
    verify test

--- InverGrove.Tests/Domain/Services/ContactServiceTests.cs
+++ InverGrove.Tests/Domain/Services/ContactServiceTests.cs
@@ -12,12 +12,23 @@ namespace InverGrove.Tests.Domain.Services
     {
         private ContactService contactService;
         private Mock<IContactRepository> mockRepository;
+        private Contact oneContact;
 
         [TestInitialize]
         public void Setup()
         {
             this.mockRepository = new Mock<IContactRepository>();
             this.contactService = new ContactService(this.mockRepository.Object);
+            this.oneContact = this.OneContact();
+        }
+
+        [TestMethod]
+        public void AddContact_CallsRepository_ExpectsVerify()
+        {
+            this.mockRepository.Setup(r => r.AddContact(It.IsAny<Contact>())).Verifiable();
+            this.contactService.AddContact(this.oneContact);
+
+            this.mockRepository.VerifyAll();
         }
 
 
