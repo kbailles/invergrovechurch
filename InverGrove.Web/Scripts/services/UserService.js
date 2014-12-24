71dad16e043709bee5e009467eba1b927db7bd29
kbailles <kbailles@outlook.com>
2014-12-23 23:27:39 -0600

    Keenan - Added a user service

---
+++ InverGrove.Web/Scripts/services/UserService.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    var appName = igchurch.constants.APP_NAME;
+
+    angular.module(appName + '.services')
+        .service('UserService', UserService);
+
+    UserService.$inject = [
+        '$http'
+    ];
+
+    function UserService($http) {
+
+    }
+})();
