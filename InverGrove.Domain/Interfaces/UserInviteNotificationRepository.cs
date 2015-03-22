30a757d58aa8b7acf74d310fd3c4341be932361d
lance22me <lancebailles@hotmail.com>
2015-03-22 12:46:53 -0500

    lance - progress save on notification repository

--- InverGrove.Domain/Interfaces/UserInviteNotificationRepository.cs
+++ InverGrove.Domain/Interfaces/UserInviteNotificationRepository.cs
@@ -1,11 +1,20 @@
-﻿namespace InverGrove.Domain.Interfaces
+﻿using InverGrove.Data;
+using InverGrove.Domain.Repositories;
+
+namespace InverGrove.Domain.Interfaces
 {
-    public class UserInviteNotificationRepository : IUserInviteNotificationRepository
+    public class UserInviteNotificationRepository : EntityRepository<Data.Entities.UserVerification, int>, IUserInviteNotificationRepository
     {
+
+        public UserInviteNotificationRepository(IInverGroveContext dataContext) 
+            : base(dataContext)
+        {
+        }
 
         public bool Add(int personId)
         {
             throw new System.NotImplementedException();
         }
+
     }
 }
