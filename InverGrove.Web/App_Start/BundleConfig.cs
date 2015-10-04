7b265a1487b1408573fd5e92f527da904590ad6c
kbailles <kbailles@outlook.com>
2015-10-04 17:29:46 -0500

    Keenan - Fixing error on login page

--- InverGrove.Web/App_Start/BundleConfig.cs
+++ InverGrove.Web/App_Start/BundleConfig.cs
@@ -44,6 +44,8 @@ namespace InverGrove.Web
                 "~/bower_components/lodash/lodash.min.js",
                 "~/bower_components/moment/min/moment.min.js",
                 "~/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js",
+                "~/bower_components/ng-file-upload/ng-file-upload-shim.min.js",
+                "~/bower_components/ng-file-upload/ng-file-upload.min.js",
 
                 "~/Scripts/common/namespace.js",
                 "~/Scripts/namespace.variables.js",
