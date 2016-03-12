aa44b210fa94b34219d30d33c123e2ca6a5c1549
kbailles <kbailles@outlook.com>
2016-03-12 10:13:08 -0600

    Keenan - Added resource page

---
+++ InverGrove.Web/Controllers/ResourceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InverGrove.Web.Controllers
+{
+    public class ResourceController : Controller
+    {
+        // GET: Resource
+        public ActionResult Index()
+        {
+            return View("_ViewResources");
+        }
+    }
+}
